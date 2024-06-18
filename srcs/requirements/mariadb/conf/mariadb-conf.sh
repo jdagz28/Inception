e08feed71c9092cc07488b364b301fe1267795c8
jdagz28 <joshdagoy28@gmail.com>
2024-06-18 12:57:17 +0200

    wordpress could not connect to mariadb

--- srcs/requirements/mariadb/conf/mariadb-conf.sh
+++ srcs/requirements/mariadb/conf/mariadb-conf.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-chsh -s $(which zsh)
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-echo "alias zshi='sh /install.sh'" >> ~/.zshrc
-
-
 # Start MariaDB service
 service mariadb start
 
